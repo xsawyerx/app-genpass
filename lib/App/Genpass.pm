0928040cefb012739c3dcb440a2b0656b056e5dd
Sawyer X <xsawyerx@cpan.org>
2009-12-07 23:06:23 +0200

    initial commit

---
+++ lib/App/Genpass.pm
@@ -0,0 +1,96 @@
+package App::Genpass;
+
+use Moose;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::Genpass - Quickly create secure passwords
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::Genpass;
+
+    my $foo = App::Genpass->new();
+    ...
+
+=head1 DESCRIPTION
+
+If you've ever needed to create 10 (or even 10,000) passwords on the fly with varying preferences (lowercase, uppercase, no confusing characters, special characters, minimum length, etc.), you know it can become a pretty pesky task.
+
+This script makes it possible to create flexible and secure passwords, quickly and easily.
+
+At some point it will support configuration files.
+
+=head1 SUBROUTINES/METHODS
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 AUTHOR
+
+Sawyer X, C<< <xsawyerx at cpan.org> >>
+
+=head1 BUGS AND LIMITATIONS
+
+Please report any bugs or feature requests to C<bug-app-genpass at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Genpass>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::Genpass
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Genpass>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Genpass>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Genpass>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Genpass/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2009 Sawyer X.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
